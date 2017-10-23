2d2e8052003665b682d643a5b56cd9018ef7f81d
barbara <barbara.krafft@gmail.com>
2017-10-23 17:04:52 +0200

    Auto stash before merge of "Dev" and "origin/Dev"
    add vehicules

--- app/src/main/java/fr/eni/ecole/projetlocation/MainActivity.java
+++ app/src/main/java/fr/eni/ecole/projetlocation/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.eni.ecole.projetlocation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,5 +46,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    public void showAddCar(MenuItem item) {
+        Intent intent = new Intent(MainActivity.this, ManageVehicule.class);
+        startActivity(intent);
     }
 }
