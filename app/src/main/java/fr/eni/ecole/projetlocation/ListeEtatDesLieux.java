3abcfabf9912add884c2b5158ff209397dc50a79
Julien <Julien.bertacco@gmail.com>
2017-10-24 16:37:53 +0200

    Auto stash before merge of "Dev" and "ProjetLocation/Dev"

--- app/src/main/java/fr/eni/ecole/projetlocation/ListeEtatDesLieux.java
+++ app/src/main/java/fr/eni/ecole/projetlocation/ListeEtatDesLieux.java
@@ -1,13 +1,31 @@
 package fr.eni.ecole.projetlocation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import fr.eni.ecole.projetlocation.dao.edl.EDLDao;
+import fr.eni.ecole.projetlocation.models.EDL;
+import fr.eni.ecole.projetlocation.models.LocationVehicule;
+
 public class ListeEtatDesLieux extends AppCompatActivity {
+    private LocationVehicule location;
+    private EDLDao edlDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_etat_des_lieux);
+        Intent intent = getIntent();
+        if(intent.hasExtra("location")){
+            location = intent.getExtras().getParcelable("location");
+        }
+        edlDao = new EDLDao(this);
+        List<EDL> edls = edlDao.getEdlByLocation(location.getId());
+
+
+
     }
 }
