c4301543faa99f237db0ba6e37b15501bf49d10f
Julien <Julien.bertacco@gmail.com>
2017-10-24 16:20:28 +0200

    Liste des etats des lieux

---
+++ app/src/main/java/fr/eni/ecole/projetlocation/ListeEtatDesLieux.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.projetlocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListeEtatDesLieux extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_etat_des_lieux);
+    }
+}
